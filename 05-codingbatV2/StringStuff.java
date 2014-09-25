649773554e30ea2648af9f67d1b5ab382a21ca5e
Javis Wu <wujavis@gmail.com>
2014-09-24 20:43:32 -0400

    added Lab made with Joseph Han

--- 05-codingbatV2/StringStuff.java
+++ 05-codingbatV2/StringStuff.java
@@ -32,6 +32,54 @@ public class StringStuff{
 	    return result;
 	}
     }
+    //nearHundred
+    public boolean nearHundred(int n) {
+	if (Math.abs(200 -n)<=  10)
+	    return true;
+	else if (Math.abs(100 -n) <= 10)
+	    return true;
+	else return false;
+    }
+    
+    //mixStart
+    public boolean mixStart(String str) {
+	
+	if (str.length() < 3) return false;  
+	
+	String two = str.substring(1, 3);
+	
+	if (two.equals("ix")) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+    
+    //TeaParty
+    public int teaParty(int tea, int candy) {
+	if (tea <5 || candy <5)
+	    return 0;
+	else if ((tea <= 0.5*candy) || (candy <= 0.5*tea))
+	    return 2;
+	else return 1;
+	
+    }
+    
+    // lastDigit
+    public boolean lastDigit(int a, int b, int c) {
+	int lasta = a%10;
+	int lastb = b%10;
+	int lastc = c%10;
+	if (lasta == lastb && lastb == lastc)
+	    return true;
+	else if (lasta == lastb)
+	    return true;
+	else if (lasta == lastc)
+	    return true;
+	else if (lastb == lastc)
+	    return true;
+	else return false;
+    }
 }
 
     
