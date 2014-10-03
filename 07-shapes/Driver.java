16d947d3dd4e1646100cfe8c17e347cb005008d4
Javis Wu <wujavis@gmail.com>
2014-10-02 22:22:26 -0400

    forgot some stuff

--- 07-shapes/Driver.java
+++ 07-shapes/Driver.java
@@ -2,7 +2,7 @@
 public class Driver {
     public static void  main(String[] args){
 	Shapes s =  new Shapes();
-	/*	System.out.println(s.box(5,4));
+	System.out.println(s.box(5,4));
 	System.out.println(s.box(4,5));
 	System.out.println(s.tri1(4));
 	System.out.println(s.tri2(4));
@@ -11,8 +11,10 @@ public class Driver {
 	System.out.println(s.tri3(4));
 	System.out.println(s.tri3(5));
 	System.out.println(s.tri4(4));
-	System.out.println(s.tri4(7));*/
+	System.out.println(s.tri4(7));
 	System.out.println(s.diamond(9));
+	System.out.println(s.diamond(5));
+	
 	
     }
 }
