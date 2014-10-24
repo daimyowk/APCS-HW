d7cc7e7442cd3d30b0c8c78fb2dfa5d56babc59c
Javis Wu <wujavis@gmail.com>
2014-10-23 21:59:39 -0400

    arrays

---
+++ 13-arrays/Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+    public static void main(String[] args){
+	stuff test= new stuff();
+	int[] testing = {1,2,13,2,3};
+	System.out.println(test.frontPiece(testing));
+	System.out.println(test.sum13(testing));
+    }
+}
+			   
+			   
