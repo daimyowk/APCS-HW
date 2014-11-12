2d6d924443e6d32b287c93c06155fa4ab87d109e
Javis Wu <wujavis@gmail.com>
2014-11-12 09:24:39 -0500

    arraylist

--- Classwork/11-12-14/Driver.java
+++ Classwork/11-12-14/Driver.java
@@ -15,7 +15,7 @@ public class Driver{
 	System.out.println(al);
 	
     
-    ArrayList<Integar> ai = new ArrayList<Integar>();
+    ArrayList<Integer> ai = new ArrayList<Integer>();
     Random r = new Random();
     for (int i=0; i<20; i++){
 	ai.add(r.nextInt(5));
