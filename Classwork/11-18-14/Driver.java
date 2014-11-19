ad009ebefb1b9072c6afaaf42efbe62855c34fb2
Javis Wu <wujavis@gmail.com>
2014-11-19 09:23:12 -0500

    horizontal adding

--- Classwork/11-18-14/Driver.java
+++ Classwork/11-18-14/Driver.java
@@ -1,6 +1,15 @@
 public class Driver{
     public static void main(String args[]){
 	wordsearch w= new wordsearch();
+	//	w.addWordH("left","hello",3,15);//should work
+	//w.addWordH("right","hello",3,34);
+	w.addWordH("left","hello",3,1);
+	/*
+	w.addWordH("look",3,14);//test ilegal overlap
+	w.addWordH("look",3,18);//test legal overlap
+	w.addWordH("look",-3,20);//test illegal roww
+	w.addWordH("look",3,55);//test illegal col
+	*/
 	System.out.println(w);
     }
 }
--- Classwork/11-18-14/wordsearch.java
+++ Classwork/11-18-14/wordsearch.java
@@ -7,17 +7,41 @@ public class wordsearch{
 	    for(int j=0;j<board[i].length;j++){
 		board[i][j]='.';
     }
+	}
+    }
     public wordsearch(){
 	this(20,40);
     }
-
+    public void addWordH(String dir, String w, int row,int col){
+	int c = col;
+	if (dir.equals("right")){
+	    if ((w.length()+c)>board[0].length){
+		c=board[0].length-w.length();
+	    }
+	for (int i=0;i< w.length();i++){
+	    board[row][c]=w.charAt(i);
+	    c++;
+	}
+	}
+	else{
+	    //not working for left
+	    if (c-(w.length())<0){
+		c=0;
+	    }
+	    for (int i=0;i< w.length();i++){
+		board[row][c]=w.charAt(i);
+		c--;
+	    }
+		
+    }
+    }
     public String toString(){
 	String s="";
 	for(int i=0;i<board.length;i++){
 	    for(int j=0;j<board[i].length;j++){
 		s=s+board[i][j];
 	    }
-	    s = s+ "/n";
+	    s = s+ "\n";
 	}
 	return s;
     }
