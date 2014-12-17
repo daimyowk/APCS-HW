4984715b625466d0b8b8f41d408cf6d63e7176d4
Javis Wu <wujavis@gmail.com>
2014-12-17 09:25:27 -0500

    class

--- Classwork/12-16-14/Gui2.java
+++ Classwork/12-16-14/Gui2.java
@@ -1,52 +1,99 @@
 import javax.swing.*;
 import java.awt.*;
-//JFrame is basic window
-//JPanel nothing
+import java.awt.event.*;
+
 public class Gui2 extends JFrame implements ActionListener {
-    private JButton b1,b2;
+    
+    private JButton b1,b2,labeler;
     private Container pane;
     private JLabel label;
-    private JTextArea text; //multiple lines
-    //JTextField one line
+    private JTextArea text;
     private JPanel canvas;
-    public void actionPerformed(){
-
+    private JCheckBox box;
+    private Canvas canvas;
+    
+    private class Canvas extends JPanel{
+	public void paintComponent (Graphics g){
+	    super.paintComponent(g);
+	    g.setColor(Color.red);
+	    g.fillOval(30,30,30,30);
+	}
     }
+    private class Key implements KeyListener{
+	public void keyPressed(KeyEvent e) {
+	    //System.out.println(e);
+	    //System.out.println();
+	}
+	public void keyReleased(KeyEvent e){}
+	public void keyTyped(KeyEvent e){
+	    //system.out.println(e);
+	    //system.out.println();
+	    if (box.isSelected()){
+		String s = text.getText();
+		s=s.toUpperCase();
+		text.setText(s);
+	    }
+	}
+    }
+    public void actionPerformed(ActionEvent e){
+	//System.out.println(e);
+	//System.out.println();
+	if (e.getSource() == b1){
+	    System.out.println("You Clicked Click me");
+	    System.out.println("And the text area has: "+text.getText());
+	}
+	else if (e.getSource() == labeler){
+	    label.setText(text.getText());
+	    // pane.add(new JLabel(text.getText()));
+	    text.setText("");
+	}
+	else if (e.getSource()==b2){
+	    System.out.println("Shutting Down");
+	    System.exit(0);
+	}
+    }
+
+    
     public Gui2(){
 	
 	setTitle("My First Gui");
 	setSize(600,600);
 	setLocation(100,100);
-	setDefaultCloseOperation(EXIT_ON_CLOSE); //allows x to close program
-
-	pane=getContentPane();
-      	//pane.setLayout(new GridLayout(3,3));
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	
+	pane = getContentPane();
+	//pane.setLayout(new GridLayout(3,3));
 	pane.setLayout(new FlowLayout());
-	b1=new JButton("Click me");
+	b1 = new JButton("Click me");
+	b1.addActionListener(this);
 	pane.add(b1);
-	b2=new JButton("exit");
+	b2 = new JButton("exit");
+	b2.addActionListener(this);
 	pane.add(b2);
-	label=new JLabel("the Label:");
+	
+	labeler= new JButton("Labler");
+	labeler.addActionListener(this);
+	pane.add(labeler);
+	label = new JLabel("The Label:");
 	pane.add(label);
-	text=new JTextArea();
+	
+	box=new JCheckBox("cap");
+	pane.add(box);
+	text =new JTextArea();
 	text.setColumns(40);
 	text.setRows(5);
 	text.setBorder(BorderFactory.createLineBorder(Color.red,2));
+	text.addKeyListener(new Key());
 	pane.add(text);
-
-	canvas=new JPanel();
+	
+	canvas = new JPanel();
 	canvas.setPreferredSize(new Dimension(300,300));
-	canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));
+	canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));		
 	pane.add(canvas);
-
 				
     }
     public static void main(String[] args) {
 	Gui2 f = new Gui2();
 	f.setVisible(true);
-	//event driven programming
-	//many possible events happen
-	//use event handlers
-	//when sometime happen(ex.button click) call something
     }
 }
